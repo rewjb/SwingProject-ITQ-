874bc3a0c1558283cc937677d2ab3bcc788d3277
RewJuBin <rewjb@naver.com>
2018-11-21 12:59:39 +0900

    zz

--- swingProject/src/joe/joe.java
+++ swingProject/src/joe/joe.java
@@ -5,6 +5,8 @@ public class joe {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("나는 정상아니다.");
+		System.out.println("나는 정상아니다.");
+		
 	}
 
 }

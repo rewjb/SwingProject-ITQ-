04c4e8c9f58b9d3d2c5ab521a022bc14a5977c8c
RewJuBin <rewjb@naver.com>
2018-11-26 13:27:41 +0900

    z

--- swingProject/src/rew/HeadFrame.java
+++ swingProject/src/rew/HeadFrame.java
@@ -4,11 +4,15 @@ import javax.swing.JFrame;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import javax.swing.JToggleButton;
+import inter.BBQHead;
+
 import javax.swing.JPanel;
 
-public class HeadFrame extends JFrame{
+public class HeadFrame extends JFrame implements BBQHead{
 	
 	JToggleButton F_OrderCheckBtn = new JToggleButton("가맹점 발주관리");
 	JToggleButton OrderBtn = new JToggleButton("발주관리");
@@ -21,6 +25,8 @@ public class HeadFrame extends JFrame{
 
 	public HeadFrame()
 	{ 
+		
+		
 		F_OrderCheckBtn.setBounds(12, 20, 130, 23); 
 		OrderBtn.setBounds(143, 20, 90, 23);
 		StockBtn.setBounds(234, 20, 110, 23);
@@ -28,12 +34,27 @@ public class HeadFrame extends JFrame{
 		FranchiseBtn.setBounds(436, 20, 100, 23);
 		SalesBtn.setBounds(537, 20, 90, 23);
 		
+		btnGroup.add(F_OrderCheckBtn);
+		btnGroup.add(OrderBtn);
+		btnGroup.add(StockBtn);
+		btnGroup.add(VenderBtn);
+		btnGroup.add(FranchiseBtn);
+		btnGroup.add(SalesBtn);
+		
 		getContentPane().add(F_OrderCheckBtn);
 		getContentPane().add(OrderBtn);
 		getContentPane().add(StockBtn);
 		getContentPane().add(VenderBtn);
 		getContentPane().add(FranchiseBtn);
 		getContentPane().add(SalesBtn);
+		
+//		F_OrderCheckBtn.addActionListener(l);
+//		OrderBtn
+//		StockBtn
+//		VenderBtn
+//		FranchiseBtn
+//		SalesBtn
+		
 		
 		//사이즈 , 레이아웃 및 각종 설정
 		getContentPane().setLayout(null);
@@ -56,4 +77,20 @@ public class HeadFrame extends JFrame{
 	public static void main(String[] args) {
 		new HeadFrame();
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }

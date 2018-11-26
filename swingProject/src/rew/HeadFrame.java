7755c25293dee7b19bb627beb9ed765219da3680
RewJuBin <rewjb@naver.com>
2018-11-26 11:19:01 +0900

    유주빈 작업중..

---
+++ swingProject/src/rew/HeadFrame.java
@@ -0,0 +1,30 @@
+package rew;
+
+import javax.swing.JFrame;
+
+public class HeadFrame extends JFrame{
+	
+	
+
+	public HeadFrame()
+	{ 
+		
+		
+		
+		
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800, 450);
+		setVisible(true);
+	}
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		new HeadFrame();
+	}
+
+}

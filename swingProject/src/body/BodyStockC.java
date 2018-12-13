0bb2ecb07036a0cb7a9dfe07d2f1d7fab7025c74
user <user@DESKTOP-7EQF3M8>
2018-12-13 14:51:14 +0900

    변수 정리

--- swingProject/src/body/BodyStockC.java
+++ swingProject/src/body/BodyStockC.java
@@ -24,19 +24,19 @@ import java.awt.Font;
 
 public class BodyStockC extends JPanel implements BodyStock, ActionListener {
 
-	private JButton button;// 미확인 재고
-	private JButton button_1;// 확인 재고
-	private JButton button_2;// 확인
+	private JButton notCheckStockBt;// 미확인 재고
+	private JButton checkStockBt;// 확인 재고
+	private JButton checkBt;// 확인
 
-	private DefaultTableModel model = new DefaultTableModel(15, 4);
-	private DefaultTableModel model1 = new DefaultTableModel(15, 2);
+	private DefaultTableModel notCheckStockmodel = new DefaultTableModel(15, 4);//미확인 재고표 
+	private DefaultTableModel checkStockmodel = new DefaultTableModel(15, 2);//확인 재고표 
 
-	private JTable listTable = new JTable(model) {//
+	private JTable notCheckStockListTable = new JTable(notCheckStockmodel) {//미확인 재고 테이블
 		public boolean isCellEditable(int row, int column) {
 			return false;
 		};
 	};
-	private JTable listTable1 = new JTable(model1)
+	private JTable checkStockListTable = new JTable(checkStockmodel)//확인 재고 테이블
 	{
 		public boolean isCellEditable(int row, int column) {
 			if (column==0) {
@@ -48,15 +48,15 @@ public class BodyStockC extends JPanel implements BodyStock, ActionListener {
 		};
 	};
 
-	private JScrollPane scroll = new JScrollPane(listTable, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+	private JScrollPane notCheckStockScroll = new JScrollPane(notCheckStockListTable, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,//미확인 재고 스크롤
 			ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-	private JScrollPane scroll1 = new JScrollPane(listTable1, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+	private JScrollPane checkStockScroll = new JScrollPane(checkStockListTable, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,//확인재고 스크롤
 			ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 	B_OrderDAO orderDAO = B_OrderDAO.getInstance();
 	B_StockDAO stockDAO = B_StockDAO.getInstance();
 
-	private final JLabel lblNewLabel = new JLabel("\uBBF8\uD655\uC778\uC7AC\uACE0 \uAD00\uB9AC");
-	private final JLabel label = new JLabel("\uD1B5\uD569\uC7AC\uACE0 \uAD00\uB9AC");
+	private final JLabel notCheckStockLabel = new JLabel("\uBBF8\uD655\uC778\uC7AC\uACE0 \uAD00\uB9AC");
+	private final JLabel checkStockLabel = new JLabel("\uD1B5\uD569\uC7AC\uACE0 \uAD00\uB9AC");
 
 	public BodyStockC() {
 //		for (int i = 0; i < model1.getRowCount(); i++) { 잘모르겠다 나중에 알아봐야 겠음.
@@ -66,44 +66,44 @@ public class BodyStockC extends JPanel implements BodyStock, ActionListener {
 		setSize(781, 360);
 		//setBackground(Color.BLUE);
 
-		button = new JButton("미확인 재고");
-		button_1 = new JButton("\uD1B5\uD569 \uC7AC\uACE0");
+		notCheckStockBt = new JButton("미확인 재고");
+		checkStockBt = new JButton("\uD1B5\uD569 \uC7AC\uACE0");
 
-		button.setBounds(47, 309, 107, 35);
-		button_1.setBounds(443, 309, 101, 34);
-		scroll.setBounds(37, 37, 283, 262);
-		scroll1.setBounds(380, 37, 223, 262);
+		notCheckStockBt.setBounds(47, 309, 107, 35);
+		checkStockBt.setBounds(443, 309, 101, 34);
+		notCheckStockScroll.setBounds(37, 37, 283, 262);
+		checkStockScroll.setBounds(380, 37, 223, 262);
 
-		listTable.getTableHeader().setResizingAllowed(false);
-		listTable.getTableHeader().setReorderingAllowed(false);
-		model.setColumnIdentifiers(new Object[] { "식자재", "수량", "본사 확인", "가맹점 확인" });
-		listTable1.getTableHeader().setResizingAllowed(false);
-		listTable1.getTableHeader().setReorderingAllowed(false);
-		model1.setColumnIdentifiers(new Object[] { "식자재", "수량" });
+		notCheckStockListTable.getTableHeader().setResizingAllowed(false);
+		notCheckStockListTable.getTableHeader().setReorderingAllowed(false);
+		notCheckStockmodel.setColumnIdentifiers(new Object[] { "식자재", "수량", "본사 확인", "가맹점 확인" });
+		checkStockListTable.getTableHeader().setResizingAllowed(false);
+		checkStockListTable.getTableHeader().setReorderingAllowed(false);
+		checkStockmodel.setColumnIdentifiers(new Object[] { "식자재", "수량" });
 
 	
-		button_2 = new JButton("\uAC00\uB9F9\uC810 \uD655\uC778");
-		button_2.setBounds(193, 309, 107, 35);
+		checkBt = new JButton("\uAC00\uB9F9\uC810 \uD655\uC778");
+		checkBt.setBounds(193, 309, 107, 35);
 
-		button.addActionListener(this);
-		button_1.addActionListener(this);
-		button_2.addActionListener(this);
-		add(scroll1);
-		add(scroll);
-		add(button);
-		add(button_1);
+		notCheckStockBt.addActionListener(this);
+		checkStockBt.addActionListener(this);
+		checkBt.addActionListener(this);
+		add(checkStockScroll);
+		add(notCheckStockScroll);
+		add(notCheckStockBt);
+		add(checkStockBt);
 
-		add(button_2);
-		lblNewLabel.setFont(new Font("나눔고딕 ExtraBold", Font.PLAIN, 15));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(121, 10, 117, 15);
+		add(checkBt);
+		notCheckStockLabel.setFont(new Font("나눔고딕 ExtraBold", Font.PLAIN, 15));
+		notCheckStockLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		notCheckStockLabel.setBounds(121, 10, 117, 15);
 		
-		add(lblNewLabel);
-		label.setHorizontalAlignment(SwingConstants.CENTER);
-		label.setFont(new Font("나눔고딕 ExtraBold", Font.PLAIN, 15));
-		label.setBounds(430, 10, 117, 15);
+		add(notCheckStockLabel);
+		checkStockLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		checkStockLabel.setFont(new Font("나눔고딕 ExtraBold", Font.PLAIN, 15));
+		checkStockLabel.setBounds(430, 10, 117, 15);
 		
-		add(label);
+		add(checkStockLabel);
 		setVisible(false);
 	}
 
@@ -119,27 +119,27 @@ public class BodyStockC extends JPanel implements BodyStock, ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == button) {// 미확인재고 버튼 기능
-			if(model.getValueAt(0, 0)==null) {
+		if (e.getSource() == notCheckStockBt) {// 미확인재고 버튼 기능
+			if(notCheckStockmodel.getValueAt(0, 0)==null) {
 				for (int i = 0; i < orderDAO.hCheckSelect(BodyFrame.id).size(); i++) {// 본사만 확인 한 재고들을 보여주는 반복문
-					model.insertRow(0, new Object[] { orderDAO.hCheckSelect(BodyFrame.id).get(i).getName(),
+					notCheckStockmodel.insertRow(0, new Object[] { orderDAO.hCheckSelect(BodyFrame.id).get(i).getName(),
 							orderDAO.hCheckSelect(BodyFrame.id).get(i).getQuantity(), orderDAO.hCheckSelect(BodyFrame.id).get(i).gethComfirm(),
 							orderDAO.hCheckSelect(BodyFrame.id).get(i).getbComfirm() });
 				}
 			}else {
-				for (int i = 0; !(model.getValueAt(0, 0)==null); i++) {
-					model.removeRow(0);
+				for (int i = 0; !(notCheckStockmodel.getValueAt(0, 0)==null); i++) {
+					notCheckStockmodel.removeRow(0);
 				}
 				for (int i = 0; i < orderDAO.hCheckSelect(BodyFrame.id).size(); i++) {
-					model.insertRow(0, new Object[] { orderDAO.hCheckSelect(BodyFrame.id).get(i).getName(),
+					notCheckStockmodel.insertRow(0, new Object[] { orderDAO.hCheckSelect(BodyFrame.id).get(i).getName(),
 							orderDAO.hCheckSelect(BodyFrame.id).get(i).getQuantity(), orderDAO.hCheckSelect(BodyFrame.id).get(i).gethComfirm(),
 							orderDAO.hCheckSelect(BodyFrame.id).get(i).getbComfirm() });
 				}
 			}
 			
-		} else if (e.getSource() == button_2) {// 확인 버튼 기능
+		} else if (e.getSource() == checkBt) {// 확인 버튼 기능
 
-			if (model.getValueAt(0, 0) == null) {// 표에 미확인재고들이 없으면 확인할수없습니다.
+			if (notCheckStockmodel.getValueAt(0, 0) == null) {// 표에 미확인재고들이 없으면 확인할수없습니다.
 				JOptionPane.showMessageDialog(null, "확인할 목록이 없습니다.");
 			} else {// 표에 미확인 재고가 있으면 확인할 수 있습니다.
 				for (int i = 0; i < orderDAO.hCheckSelect(BodyFrame.id).size(); i++) {// 확인버튼 누를때 발주dB에 있는 데이터를 재고 DB로 옮기는 반복문
@@ -151,45 +151,45 @@ public class BodyStockC extends JPanel implements BodyStock, ActionListener {
 //				}
 				orderDAO.bConfirmUpdate();// 가맹점 확인 메서드
 				for (int i = 0; i>=0; i++) {
-					if(!(model.getValueAt(i, 0)==null)) {
-						model.setValueAt("bk-1", i, 3);
+					if(!(notCheckStockmodel.getValueAt(i, 0)==null)) {
+						notCheckStockmodel.setValueAt("bk-1", i, 3);
 					}else {
 						break;
 					}
 				}
-				if (!(model1.getValueAt(0, 0) == null)) {
-					for (int i = 0; !(model1.getValueAt(0, 0) == null); i++) {// 확인재고 버튼을 누를때마다 실시간으로 업데이트 하기위해 표를 지워주는 반복문
-						model1.removeRow(0);
+				if (!(checkStockmodel.getValueAt(0, 0) == null)) {
+					for (int i = 0; !(checkStockmodel.getValueAt(0, 0) == null); i++) {// 확인재고 버튼을 누를때마다 실시간으로 업데이트 하기위해 표를 지워주는 반복문
+						checkStockmodel.removeRow(0);
 
 					}
 					for (int i = 0; i < stockDAO.stockSelectAll(BodyFrame.id).size(); i++) {
-						model1.insertRow(0, new Object[] { stockDAO.stockSelectAll(BodyFrame.id).get(i).getName(),
+						checkStockmodel.insertRow(0, new Object[] { stockDAO.stockSelectAll(BodyFrame.id).get(i).getName(),
 								stockDAO.stockSelectAll(BodyFrame.id).get(i).getQuantity() });
 					}
 				} else {
 
 					for (int i = 0; i < stockDAO.stockSelectAll(BodyFrame.id).size(); i++) {
-						model1.insertRow(0, new Object[] { stockDAO.stockSelectAll(BodyFrame.id).get(i).getName(),
+						checkStockmodel.insertRow(0, new Object[] { stockDAO.stockSelectAll(BodyFrame.id).get(i).getName(),
 								stockDAO.stockSelectAll(BodyFrame.id).get(i).getQuantity() });
 					}
 				}
 					
 			}
 
-		} else if (e.getSource() == button_1) {// 통합재고 버튼 기능
-			if (!(model1.getValueAt(0, 0) == null)) {
-				for (int i = 0; !(model1.getValueAt(0, 0) == null); i++) {// 통합재고 버튼을 누를때마다 실시간으로 업데이트 하기위해 표를 지워주는 반복문
-					model1.removeRow(0);
+		} else if (e.getSource() == checkStockBt) {// 통합재고 버튼 기능
+			if (!(checkStockmodel.getValueAt(0, 0) == null)) {
+				for (int i = 0; !(checkStockmodel.getValueAt(0, 0) == null); i++) {// 통합재고 버튼을 누를때마다 실시간으로 업데이트 하기위해 표를 지워주는 반복문
+					checkStockmodel.removeRow(0);
 
 				}
 				for (int i = 0; i < stockDAO.stockSelectAll(BodyFrame.id).size(); i++) {
-					model1.insertRow(0, new Object[] { stockDAO.stockSelectAll(BodyFrame.id).get(i).getName(),
+					checkStockmodel.insertRow(0, new Object[] { stockDAO.stockSelectAll(BodyFrame.id).get(i).getName(),
 							stockDAO.stockSelectAll(BodyFrame.id).get(i).getQuantity() });
 				}
 			} else {
 
 				for (int i = 0; i < stockDAO.stockSelectAll(BodyFrame.id).size(); i++) {
-					model1.insertRow(0, new Object[] { stockDAO.stockSelectAll(BodyFrame.id).get(i).getName(),
+					checkStockmodel.insertRow(0, new Object[] { stockDAO.stockSelectAll(BodyFrame.id).get(i).getName(),
 							stockDAO.stockSelectAll(BodyFrame.id).get(i).getQuantity() });
 				}
 			}
--- swingProject/src/body/HallC.java
+++ swingProject/src/body/HallC.java
@@ -42,25 +42,16 @@ import javax.swing.SwingConstants;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class HallC extends JPanel implements ActionListener, BodyHall, ItemListener {
+public class HallC extends JPanel implements ActionListener, BodyHall{
 
-	// 버튼그룹
-	private ButtonGroup bg;
-	// 계산방법 다이얼로그
-	private JDialog dialog = new JDialog();
-	// 결제방법 선택한 후 결제 버튼
-	private JButton paymenBt;
-	// 체크할 수 있는 라디오 버튼
-	private JRadioButton radiobt;
-	private JRadioButton radiobt1;
 
 	// 테이블 버튼
-	private JButton jb;// 1번테이블 버튼
-	private JButton jb1;// 2번테이블 버튼
-	private JButton jb2;// 3번테이블 버튼
-	private JButton jb3;// 4번 테이블 버튼
-	private JButton jb4;// 5번 테이블 버튼
-	private JButton jb5;// 6번 테이블 버튼
+	private JButton jb1;// 1번테이블 버튼
+	private JButton jb2;// 2번테이블 버튼
+	private JButton jb3;// 3번테이블 버튼
+	private JButton jb4;// 4번 테이블 버튼
+	private JButton jb5;// 5번 테이블 버튼
+	private JButton jb6;// 6번 테이블 버튼
 
 	// 기본 테이블 모델
 	private DefaultTableModel model1 = new DefaultTableModel(15, 2);// 1번테이블 표
@@ -71,7 +62,7 @@ public class HallC extends JPanel implements ActionListener, BodyHall, ItemListe
 	private DefaultTableModel model6 = new DefaultTableModel(15, 2);// 6번테이블 표
 
 	// 계산 버튼
-	private JButton button;// 주문하기 버튼
+	private JButton orderButton;// 주문하기 버튼
 	private JButton button1;// 1번 테이블 계산 버튼
 	private JButton button2;// 2번 테이블 계산 버튼
 	private JButton button3;// 3번 테이블 계산 버튼
@@ -79,17 +70,17 @@ public class HallC extends JPanel implements ActionListener, BodyHall, ItemListe
 	private JButton button5;// 5번 테이블 계산 버튼
 	private JButton button6;// 6번 테이블 계산 버튼
 	// 주문 선택버튼
-	private JButton button7;// 선택하기 버튼
+	private JButton selectButton;// 선택하기 버튼
 	// 버튼 그룹
 	// private ButtonGroup bg;
 
 	HashMap<JButton, DefaultTableModel> key = new HashMap<>();// 해쉬맵 사용법
 
 	// 테이블내 표
-	private DefaultTableModel model = new DefaultTableModel(15, 2);
+	private DefaultTableModel orderModel = new DefaultTableModel(15, 2);//주문하기 표
 
 	// 표가 들어갈 테이블
-	private JTable listTable = new JTable(model) {
+	private JTable listTable = new JTable(orderModel) {
 		public boolean isCellEditable(int row, int column) {
 			return false;
 		};
@@ -148,13 +139,13 @@ public class HallC extends JPanel implements ActionListener, BodyHall, ItemListe
 																											// 스크롤바
 			ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 	// 주문할 프레임
-	private JFrame jframe;
+	private JFrame orderFrame;
 	// 메뉴선택 콤보박스
-	private JComboBox combo;
+	private JComboBox menuCombo;
 
 	private JTextField textfiled;
-	private JLabel jl;
-	private JLabel jl_1;
+	private JLabel menuLabel;
+	private JLabel quantityLabel;
 
 	// 각버튼의 주소값을 담아두는 변수
 	Object temp = null;
@@ -171,62 +162,54 @@ public class HallC extends JPanel implements ActionListener, BodyHall, ItemListe
 	String[] menu = new String[] { "후라이드", "양념", "간장", "음료" };// 메뉴 리스트
 
 	public HallC() {// 생성자
-		bg = new ButtonGroup();
-
-		radiobt = new JRadioButton("카드");
-		radiobt1 = new JRadioButton("현금");
-		paymenBt = new JButton("결제");
-
-		bg.add(radiobt);
-		bg.add(radiobt1);
 
 		// 각 테이블 버튼 객체 생성
-		jb = new JButton("1번 테이블");
-		jb1 = new JButton("2번 테이블");
-		jb2 = new JButton("3번 테이블");
-		jb3 = new JButton("4번 테이블");
-		jb4 = new JButton("5번 테이블");
-		jb5 = new JButton("6번 테이블");
+		jb1 = new JButton("1번 테이블");
+		jb2 = new JButton("2번 테이블");
+		jb3 = new JButton("3번 테이블");
+		jb4 = new JButton("4번 테이블");
+		jb5 = new JButton("5번 테이블");
+		jb6 = new JButton("6번 테이블");
 		// 각 테이블 버튼이랑 테이블 모델 짝 맞춰주기
-		key.put(jb, model1);
-		key.put(jb1, model2);
-		key.put(jb2, model3);
-		key.put(jb3, model4);
-		key.put(jb4, model5);
-		key.put(jb5, model6);
+		key.put(jb1, model1);
+		key.put(jb2, model2);
+		key.put(jb3, model3);
+		key.put(jb4, model4);
+		key.put(jb5, model5);
+		key.put(jb6, model6);
 		// 프레임 사이즈 및 레이아웃 설정
 		setSize(790, 388);
 		setLayout(null);
 		// 버튼 객체생성 및 위치지정
-		button = new JButton("주문하기");
-		button.setBounds(150, 210, 100, 30);
+		orderButton = new JButton("주문하기");
+		orderButton.setBounds(150, 210, 100, 30);
 		// 라벨 객체생성 및 위치지정
-		jl = new JLabel("메뉴  ");
-		jl.setBounds(15, 10, 50, 30);
-		jl_1 = new JLabel("수량 ");
-		jl_1.setBounds(155, 10, 50, 30);
+		menuLabel = new JLabel("메뉴  ");
+		menuLabel.setBounds(15, 10, 50, 30);
+		quantityLabel = new JLabel("수량 ");
+		quantityLabel.setBounds(155, 10, 50, 30);
 		// 텍스트 필드 객체생성 및 위치지정
 		textfiled = new JTextField();
 		textfiled.setBounds(190, 10, 90, 30);
 		// 콤보박스 객체생성, 메뉴리스트 넣기,위치지정
-		combo = new JComboBox(menu);
-		combo.setBounds(50, 10, 90, 30);
+		menuCombo = new JComboBox(menu);
+		menuCombo.setBounds(50, 10, 90, 30);
 		// 버튼 객체생성 및 위치지정
-		button7 = new JButton("선택하기");
-		button7.setBounds(30, 210, 100, 30);
+		selectButton = new JButton("선택하기");
+		selectButton.setBounds(30, 210, 100, 30);
 
 		// 주문창 프레임 객체 생성 ,레이아웃설정,위치지정,프레임위에 올릴 컴포넌트 넣기
-		jframe = new JFrame("주문하기");
-		jframe.getContentPane().setLayout(null);
-		jframe.setBounds(250, 295, 300, 300);
-		jframe.getContentPane().add(scroll);
-		jframe.getContentPane().add(combo);
-		jframe.getContentPane().add(textfiled);
-		jframe.getContentPane().add(jl);
-		jframe.getContentPane().add(jl_1);
-		jframe.getContentPane().add(button);
-		jframe.getContentPane().add(button7);
-		jframe.setVisible(false);
+		orderFrame = new JFrame("주문하기");
+		orderFrame.getContentPane().setLayout(null);
+		orderFrame.setBounds(250, 295, 300, 300);
+		orderFrame.getContentPane().add(scroll);
+		orderFrame.getContentPane().add(menuCombo);
+		orderFrame.getContentPane().add(textfiled);
+		orderFrame.getContentPane().add(menuLabel);
+		orderFrame.getContentPane().add(quantityLabel);
+		orderFrame.getContentPane().add(orderButton);
+		orderFrame.getContentPane().add(selectButton);
+		orderFrame.setVisible(false);
 		// 버튼 객체생성 및 위치 지정
 		button1 = new JButton("계산");
 		button1.setBounds(68, 166, 60, 24);
@@ -249,28 +232,15 @@ public class HallC extends JPanel implements ActionListener, BodyHall, ItemListe
 		scroll6.setBounds(530, 235, 144, 97);
 		scroll.setBounds(0, 50, 285, 145);
 
-		// 다이얼 로그 설정
-		dialog.setTitle("계산방법");
-		dialog.setLayout(null);
-		dialog.setBounds(250, 295, 300, 300);
-		dialog.setVisible(false);
-		dialog.add(radiobt);
-		dialog.add(radiobt1);
-		dialog.add(paymenBt);
-
-		radiobt.setBounds(80, 10, 70, 50);
-		radiobt1.setBounds(150, 10, 70, 50);
-		paymenBt.setBounds(110, 180, 70, 30);
-
 		// 버튼 위치 지정
-		jb.setBounds(91, 27, 97, 28);
-		jb1.setBounds(325, 27, 97, 29);
-		jb2.setBounds(559, 27, 97, 29);
-		jb3.setBounds(71, 201, 97, 29);
-		jb4.setBounds(325, 201, 97, 29);
-		jb5.setBounds(559, 201, 97, 29);
+		jb1.setBounds(91, 27, 97, 28);
+		jb2.setBounds(325, 27, 97, 29);
+		jb3.setBounds(559, 27, 97, 29);
+		jb4.setBounds(71, 201, 97, 29);
+		jb5.setBounds(325, 201, 97, 29);
+		jb6.setBounds(559, 201, 97, 29);
 
-		model.setColumnIdentifiers(new Object[] { "메뉴", "수량" });// 주문하기 표 컬럼
+		orderModel.setColumnIdentifiers(new Object[] { "메뉴", "수량" });// 주문하기 표 컬럼
 		model1.setColumnIdentifiers(new Object[] { "메뉴", "수량" });// 1번테이블 컬럼
 		model2.setColumnIdentifiers(new Object[] { "메뉴", "수량" });// 2번테이블 컬럼
 		model3.setColumnIdentifiers(new Object[] { "메뉴", "수량" });// 3번테이블 컬럼
@@ -279,17 +249,17 @@ public class HallC extends JPanel implements ActionListener, BodyHall, ItemListe
 		model6.setColumnIdentifiers(new Object[] { "메뉴", "수량" });// 6번테이블 컬럼
 
 		// 각 컴포넌트 jpanel위에 올리기
-		add(jb4);
 		add(jb5);
+		add(jb6);
 		add(scroll1);
-		add(jb3);
+		add(jb4);
 		add(scroll3);
 		add(scroll2);
-		add(jb2);
-		add(scroll4);
 		add(jb1);
+		add(scroll4);
+		add(jb2);
 		add(scroll5);
-		add(jb);
+		add(jb1);
 		add(scroll6);
 		add(button1);
 		add(button6);
@@ -298,23 +268,21 @@ public class HallC extends JPanel implements ActionListener, BodyHall, ItemListe
 		add(button4);
 		add(button5);
 
-		jb.addActionListener(this);// 1번 테이블 선택시
-		jb1.addActionListener(this);// 2번 테이블 선택시
-		jb2.addActionListener(this);// 3번 테이블 선택시
-		jb3.addActionListener(this);// 4번 테이블 선택시
-		jb4.addActionListener(this);// 5번 테이블 선택시
-		jb5.addActionListener(this);// 6번 테이블 선택시
+		jb1.addActionListener(this);// 1번 테이블 선택시
+		jb2.addActionListener(this);// 2번 테이블 선택시
+		jb3.addActionListener(this);// 3번 테이블 선택시
+		jb4.addActionListener(this);// 4번 테이블 선택시
+		jb5.addActionListener(this);// 5번 테이블 선택시
+		jb6.addActionListener(this);// 6번 테이블 선택시
 
-		button.addActionListener(this); // 주문하기 버튼
+		orderButton.addActionListener(this); // 주문하기 버튼
 		button1.addActionListener(this);// 1번 테이블 계산
 		button2.addActionListener(this);// 2번 테이블 계산
 		button3.addActionListener(this);// 3번 테이블 계산
 		button4.addActionListener(this);// 4번 테이블 계산
 		button5.addActionListener(this);// 5번 테이블 계산
 		button6.addActionListener(this);// 6번 테이블 계산
-		button7.addActionListener(this);// 선택하기 버튼 누를때
-		radiobt.addItemListener(this);
-		radiobt1.addItemListener(this);
+		selectButton.addActionListener(this);// 선택하기 버튼 누를때
 
 		setVisible(false);
 	}
@@ -366,73 +334,67 @@ public class HallC extends JPanel implements ActionListener, BodyHall, ItemListe
 	public void actionPerformed(ActionEvent e) {
 		// System.out.println((String)combo.getSelectedItem());콤보박스 선택값 출력
 
-		if (e.getSource() == jb) {
-			jframe.setVisible(true);
+		if (e.getSource() == jb1) {
+			orderFrame.setVisible(true);
 			temp = e.getSource();
-			jframe.setTitle("1번테이블");
-		} else if (e.getSource() == jb1) {
-			jframe.setVisible(true);
-			temp = e.getSource();
-			jframe.setTitle("2번테이블");
+			orderFrame.setTitle("1번테이블");
 		} else if (e.getSource() == jb2) {
-			jframe.setVisible(true);
+			orderFrame.setVisible(true);
 			temp = e.getSource();
-			jframe.setTitle("3번테이블");
+			orderFrame.setTitle("2번테이블");
 		} else if (e.getSource() == jb3) {
-			jframe.setVisible(true);
+			orderFrame.setVisible(true);
 			temp = e.getSource();
-			jframe.setTitle("4번테이블");
+			orderFrame.setTitle("3번테이블");
 		} else if (e.getSource() == jb4) {
-			jframe.setVisible(true);
+			orderFrame.setVisible(true);
 			temp = e.getSource();
-			jframe.setTitle("5번테이블");
+			orderFrame.setTitle("4번테이블");
 		} else if (e.getSource() == jb5) {
-			jframe.setVisible(true);
+			orderFrame.setVisible(true);
 			temp = e.getSource();
-			jframe.setTitle("6번테이블");
-		} else if (e.getSource() == button) {// 주문하기
+			orderFrame.setTitle("5번테이블");
+		} else if (e.getSource() == jb6) {
+			orderFrame.setVisible(true);
+			temp = e.getSource();
+			orderFrame.setTitle("6번테이블");
+		} else if (e.getSource() == orderButton) {// 주문하기
 
-			if (model.getValueAt(0, 0) == null) {
+			if (orderModel.getValueAt(0, 0) == null) {
 				JOptionPane.showMessageDialog(null, "주문목록이 없습니다.");
 			} else {
 				while (true) {
 					DefaultTableModel temp2 = key.get(temp);
-					temp2.insertRow(0, new Object[] { model.getValueAt(0, 0), model.getValueAt(0, 1) });
-					model.removeRow(0);
+					temp2.insertRow(0, new Object[] { orderModel.getValueAt(0, 0), orderModel.getValueAt(0, 1) });
+					orderModel.removeRow(0);
 
-					if (model.getValueAt(0, 0) == null) {
+					if (orderModel.getValueAt(0, 0) == null) {
 						break;
 					}
 				}
 			}
 
 		} else if (e.getSource() == button1) {// 1번테이블 계산
-			dialog.setVisible(true);
-			jframe.setVisible(false);
+			orderFrame.setVisible(false);
 			inputSales(model1);
 		} else if (e.getSource() == button2) {// 2번테이블 계산
-			dialog.setVisible(true);
-			jframe.setVisible(false);
+			orderFrame.setVisible(false);
 			inputSales(model2);
 		} else if (e.getSource() == button3) {// 3번테이블 계산
-			dialog.setVisible(true);
-			jframe.setVisible(false);
+			orderFrame.setVisible(false);
 			inputSales(model3);
 		} else if (e.getSource() == button4) {// 4번테이블 계산
-			dialog.setVisible(true);
-			jframe.setVisible(false);
+			orderFrame.setVisible(false);
 			inputSales(model4);
 		} else if (e.getSource() == button5) {// 5번테이블 계산
-			dialog.setVisible(true);
-			jframe.setVisible(false);
+			orderFrame.setVisible(false);
 			inputSales(model5);
 		} else if (e.getSource() == button6) {// 6번테이블 계산
-			dialog.setVisible(true);
-			jframe.setVisible(false);
+			orderFrame.setVisible(false);
 			inputSales(model6);
-		} else if (e.getSource() == button7) {// 선택하기 버튼 누를시
+		} else if (e.getSource() == selectButton) {// 선택하기 버튼 누를시
 			if (!(textfiled.getText().equals(""))) {
-				model.insertRow(0, new Object[] { (String) combo.getSelectedItem(), textfiled.getText() });
+				orderModel.insertRow(0, new Object[] { (String) menuCombo.getSelectedItem(), textfiled.getText() });
 			} else {
 				JOptionPane.showMessageDialog(null, "수량을 입력해주세요");
 			}
@@ -449,11 +411,5 @@ public class HallC extends JPanel implements ActionListener, BodyHall, ItemListe
 		((Component) bbqBody).setVisible(false);
 	}
 
-	@Override
-	public void itemStateChanged(ItemEvent e) {
-		if (radiobt.isSelected()) {
-			JOptionPane.showMessageDialog(null, "핑신");
-		}
-
-	}
+	
 }

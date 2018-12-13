6f582d287c7955e82e6821ce4580c46aaa3a5f5e
user <user@DESKTOP-7EQF3M8>
2018-12-13 09:20:24 +0900

    ㅇ

--- swingProject/src/DTO_DAO/B_OrderDAO.java
+++ swingProject/src/DTO_DAO/B_OrderDAO.java
@@ -109,7 +109,7 @@ public class B_OrderDAO {
 
 		try {
 			connectDB();
-			sql = "SELECT * from bodyorder where id = '"+id+"'";
+			sql = "SELECT * from bodyorder where id = '"+id+"' order by hconfirm = ''";
 			ps = con.prepareStatement(sql);
 			rs = ps.executeQuery();
 

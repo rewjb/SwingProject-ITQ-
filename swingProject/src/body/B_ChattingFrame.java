03d142cdc1d2a851aa5654a61124ed0f1fa6a804
user <user@DESKTOP-7EQF3M8>
2018-12-18 10:01:19 +0900

    dsa

--- swingProject/src/body/B_ChattingFrame.java
+++ swingProject/src/body/B_ChattingFrame.java
@@ -1,6 +1,7 @@
 package body;
 
 import java.awt.BorderLayout;
+
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -26,6 +27,29 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.table.DefaultTableModel;
+
+//1. 계층도
+//하위 내용은 클래스 계층도 입니다.
+//기본적으로 인터페이스 계층도 이며 () 괄호안에 들어가 있는 것은 인터페이스 및 클래스 여부입니다.
+//1) 기호정리
+//   괄호 :  I(인터페이스) , C(클래스)
+//    ↑ : 아래에 위치한 클래스를 위에 있는 클래스에서 객체로 만들어 사용
+//    │ : 상속 및 구현
+//                                                                                                BBQ(I)                                                                          
+//                                                       ┌───────────────────────────────────────────┴──────────────────────────────────────────────────────────────────────────────────────────┐
+//                                                   BBQHead(I)                                                                                                                             BBQBody(I) 
+//     ┌───────────────────┌────────────────────┌────────┴────────────┐──────────────────┐─────────────────┐────────────────┐──────────────────────────┐                   ┌────────────┌───────┴───────┐───────────────┐ 
+//HeadCheckOrder(I)    HeadOrder(I)       HeadStockInOut(I)       HeadVender(I)  HeadVenderProduct(I) HeadFranchise(I)   HeadSales(I)            H_ChattingFrame(C)     BodyHall(I)   BodyOrder(I)   BodySales(I)   BodyStock(I)
+//     │                   │                    │                     │                  │                 │                │                          ↑                   │            │               │               │
+//H_CheckOrder(C)      H_Order(C)         H_StockInOut(C)         H_Vender(C)       H_V_Product(C)    H_Franchise(C)     H_Salses(C)            ┌───────│─────────────┐ B_HallC(C)   B_OrderC(C)     B_SalesC(C)     B_StockC(C)
+//                                                                                                            ┌─────────────↑────────────────┐ │H_ChattingJoin(C)    │                     ┌────────────↑─────────────┐
+//                                                                                                            │ H_Salses_BodySalesBarChart(C)│ │       ↑             │                     │B_SalesC_SalesDataChart(C)│ 
+//                                                                                                            │ H_Salses_BodySalesPieChart(C)│ │H_ChattingrManager(C)│                     └──────────────────────────┘
+//                                                                                                            │H_Salses_HeadSalsesBarChart(C)│ └─────────────────────┘
+//                                                                                                            └──────────────────────────────┘
+//2. 변수명
+//1) 전역변수 식별자 특징
+//  => g_식별자  (g : 글로벌 변수)  ,  ex) g_BtnSelect
 
 public class B_ChattingFrame extends JDialog implements ActionListener {
 
--- swingProject/src/body/B_Frame.java
+++ swingProject/src/body/B_Frame.java
@@ -33,6 +33,30 @@ import javax.swing.JPanel;
 // 아래의 4번은 제작한 모든  Jpanel을 3번까지의 과정을 거친 후 4번을 실행한다.
 // 4) 3번이 모든 부품이 입력되었다면 마지막으로 hide 메서드로 나머지를 감추는 마무리 작업을 한다.
 
+
+//1. 계층도
+//하위 내용은 클래스 계층도 입니다.
+//기본적으로 인터페이스 계층도 이며 () 괄호안에 들어가 있는 것은 인터페이스 및 클래스 여부입니다.
+//1) 기호정리
+//   괄호 :  I(인터페이스) , C(클래스)
+//    ↑ : 아래에 위치한 클래스를 위에 있는 클래스에서 객체로 만들어 사용
+//    │ : 상속 및 구현
+//                                                                                                BBQ(I)                                                                          
+//                                                       ┌───────────────────────────────────────────┴──────────────────────────────────────────────────────────────────────────────────────────┐
+//                                                   BBQHead(I)                                                                                                                             BBQBody(I) 
+//     ┌───────────────────┌────────────────────┌────────┴────────────┐──────────────────┐─────────────────┐────────────────┐──────────────────────────┐                   ┌────────────┌───────┴───────┐───────────────┐ 
+//HeadCheckOrder(I)    HeadOrder(I)       HeadStockInOut(I)       HeadVender(I)  HeadVenderProduct(I) HeadFranchise(I)   HeadSales(I)            H_ChattingFrame(C)     BodyHall(I)   BodyOrder(I)   BodySales(I)   BodyStock(I)
+//     │                   │                    │                     │                  │                 │                │                          ↑                   │            │               │               │
+//H_CheckOrder(C)      H_Order(C)         H_StockInOut(C)         H_Vender(C)       H_V_Product(C)    H_Franchise(C)     H_Salses(C)            ┌───────│─────────────┐ B_HallC(C)   B_OrderC(C)     B_SalesC(C)     B_StockC(C)
+//                                                                                                            ┌─────────────↑────────────────┐ │H_ChattingJoin(C)    │                     ┌────────────↑─────────────┐
+//                                                                                                            │ H_Salses_BodySalesBarChart(C)│ │       ↑             │                     │B_SalesC_SalesDataChart(C)│ 
+//                                                                                                            │ H_Salses_BodySalesPieChart(C)│ │H_ChattingrManager(C)│                     └──────────────────────────┘
+//                                                                                                            │H_Salses_HeadSalsesBarChart(C)│ └─────────────────────┘
+//                                                                                                            └──────────────────────────────┘
+//2. 변수명
+//1) 전역변수 식별자 특징
+//  => g_식별자  (g : 글로벌 변수)  ,  ex) g_BtnSelect
+
 public class B_Frame extends JFrame implements BBQBody, ActionListener {
 
 	JToggleButton g_OrderBtn = new JToggleButton("발주관리",new ImageIcon("img/발주관리 아이콘.png"));

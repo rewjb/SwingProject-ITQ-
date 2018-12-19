baa0c4e6b03421f3151a7aa73c1351338f85ca71
RewJuBin <rewjb@naver.com>
2018-12-19 10:14:58 +0900

    zz

--- swingProject/src/inter/HeadStockInOut.java
+++ swingProject/src/inter/HeadStockInOut.java
@@ -2,4 +2,11 @@ package inter;
 
 public interface HeadStockInOut extends BBQHead{
 
-}
+	
+	public void insertPointList(String point);
+	// 해당 포인트에 해당하는 재고 테이블 넣기
+
+	public void  insertInOutTotalStockHistoryList();
+	// 입출고 및 전체 재고 테이블에 재고 넣기
+	
+}// 클래스 종료

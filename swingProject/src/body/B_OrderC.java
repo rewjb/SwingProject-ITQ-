ee0da1a0cf7e955a075586ce6dc9d8dfc0a72ec8
user <user@DESKTOP-7EQF3M8>
2018-12-19 10:04:12 +0900

    발주취소 수정

--- swingProject/src/body/B_OrderC.java
+++ swingProject/src/body/B_OrderC.java
@@ -331,9 +331,17 @@ public class B_OrderC extends JPanel implements BodyOrder, ActionListener, ItemL
 	
 	@Override
 	public void orderDelete() {//발주 취소 메서드
+		int checkCount = 0;
 		g_Selects = g_StockListTable.getSelectedRows();
 		for (int i = 0; i < g_Selects.length; i++) {
-			if (g_OrderListModel.getValueAt(g_Selects[i], 3).equals("")) {
+			if (g_OrderListModel.getValueAt(g_Selects[i], 3).equals("ck_1")) {
+				JOptionPane.showMessageDialog(null, "본사확인이 완료된 발주는 취소할 수 없습니다.");
+				checkCount++;
+				break;
+			}
+		}
+		for (int i = 0; i < g_Selects.length; i++) {
+			if (g_OrderListModel.getValueAt(g_Selects[i], 3).equals("") && checkCount == 0 ) {
 				B_OrderDAO.getInstance().orderDelete(g_ListNum.get(g_Selects[i]));
 			}
 		}
